8ea4aa3edbbebf9cceda92dc8bfe210ce7399a43
Eshil Patel <xeshil101@optonline.net>
2019-03-14 18:32:18 -0400

    initial skeleton of project, with the worksplit

---
+++ photos/Photos/src/controller/LoginController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	@FXML
+	private TextField username;
+	@FXML
+	private Button login;
+}
