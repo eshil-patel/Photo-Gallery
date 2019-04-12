613940dbe46d93c4ad722a66f86a3dbc884e73b3
Eshil Patel <xeshil101@optonline.net>
2019-04-12 18:55:16 -0400

    small change to datasaver

--- photos/Photos/src/controller/DataSaver.java
+++ photos/Photos/src/controller/DataSaver.java
@@ -13,17 +13,17 @@ public class DataSaver {
 	public static void save(UserList ULL){
 		try
         {    
-            System.out.println("Entered the try condition");
+            //System.out.println("Entered the try condition");
             String pa = System.getProperty("user.dir")+File.separator+"src"+File.separator+"model";
-			System.out.println("ADMIN CONT " + pa);
+			//System.out.println("ADMIN CONT " + pa);
             FileOutputStream file = new FileOutputStream(pa+File.separator+"UserData.dat"); 
-            System.out.println("the filepath was something that was recognized");
+            //System.out.println("the filepath was something that was recognized");
             ObjectOutputStream out = new ObjectOutputStream(file);  
             // Method for serialization of object 
             out.writeObject(ULL); 
             out.close(); 
             file.close(); 
-            System.out.println("Object has been serialized"); 
+            //System.out.println("Object has been serialized"); 
             
         } 
           
@@ -35,19 +35,21 @@ public class DataSaver {
 		UserList UL = null;
 		try {
 			String pa = System.getProperty("user.dir")+File.separator+"src"+File.separator+"model" + File.separator + "UserData.dat";
-			System.out.println(pa);
+			//System.out.println(pa);
 			FileInputStream FIS = new FileInputStream(pa);
-			System.out.println("FIS CREATED" + FIS.available());
+			//System.out.println("FIS CREATED" + FIS.available());
 			if (FIS.available() == 0){
 				UL = new UserList();
+				FIS.close();
 				return UL;
 			}
-			System.out.println("FIS NOT NULL");
+			//System.out.println("FIS NOT NULL");
 			ObjectInputStream in = new ObjectInputStream(FIS);
-			System.out.println("after the output stream read");
+			//System.out.println("after the output stream read");
 	        UL = (UserList) in.readObject(); 
-	        System.out.println(UL.toString());
-	        System.out.println("The deserialzation of the empty list worked");
+	        //System.out.println(UL.toString());
+	        //System.out.println("The deserialzation of the empty list worked");
+	        FIS.close();
 	        in.close();
 	        return UL;
 		}
