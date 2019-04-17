2d131d54d6f1dbbbf279e2f8b7a7664edbec3e39
Eshil Patel <xeshil101@optonline.net>
2019-04-16 20:50:13 -0400

    adding in javadoc tags

--- photos/Photos/src/model/Photo.java
+++ photos/Photos/src/model/Photo.java
@@ -21,39 +21,68 @@ public class Photo implements Serializable{
 	private ArrayList <Tag> Tags;
 	private Date date;
 	private String Caption;
+	/**Constructor for photo
+	 * @param path
+	 * @param longdate
+	 */
 	public Photo(String path,long longdate){
 		this.path = path;
 		this.date= new Date(longdate);
 		Tags = new ArrayList<Tag>();
 		Caption = null;
 	}
+	/**Alternate constructor for photo
+	 * @param path
+	 * @param date
+	 */
 	public Photo(String path, Date date){
 		this.path = path;
 		this.date= date;
 		Tags = new ArrayList<Tag>();
 		Caption = null;
 	}
+	/**Returns the filepath where the image is located 
+	 * @return
+	 */
 	public String getPath() {
 		return path;
 	}
+	/**Set the filepath of the image 
+	 * @param path
+	 */
 	public void setPath(String path) {
 		this.path = path;
 	}
+	/**Returns the date of photo
+	 * @return
+	 */
 	public Date getDate() {
 		return date;
 	}
+	/**Gets the caption of the photo
+	 * @return
+	 */
 	public String getCaption() {
 		return Caption;
 	}
+	/**Set the caption for the photo
+	 * @param caption
+	 */
 	public void setCaption(String caption) {
 		Caption = caption;
 	}
+	/**Add tag to the photo 
+	 * @param m
+	 */
 	public void addTags(Tag m){
 		if (Tags.contains(m)){
 			return;
 		}
 		Tags.add(m);
 	}
+	/**Remove tag from the photo
+	 * @param m
+	 */
 	public void removeTags(Tag m){
 		if (!(Tags.contains(m))){
 			return;
@@ -61,15 +90,26 @@ public class Photo implements Serializable{
 		Tags.remove(m);
 	}
 	
+	/**Get the list of tags from the photo
+	 * @return
+	 */
 	public ArrayList<Tag> getTags() {
 		return Tags;
 	}
+	/**Check if the photo has specific tag 
+	 * @param m
+	 * @return
+	 */
 	public boolean hasTag(Tag m){
 		if (Tags.contains(m)){
 			return true;
 		}
 		return false;
 	}
+	/* (non-Javadoc)
+	 * equals method customized 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	public boolean equals(Object a) {		
 		if (a.getClass() != this.getClass()){
 			return false;
@@ -80,10 +120,17 @@ public class Photo implements Serializable{
 		}
 		return false;
 	}
+	/**set date of the photo
+	 * @param text
+	 */
 	public void setDate(String text) {
 		// TODO Auto-generated method stub
 		date = new Date(text);
 	}
+	/* (non-Javadoc)
+	 * toString method customized 
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString(){
 		return this.getPath();
 	}
--- photos/Photos/src/model/UserList.java
+++ photos/Photos/src/model/UserList.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class UserList implements Serializable{
-	/**
+	/**Constructor for Userlist 
 	 * 
 	 */
 	private static final long serialVersionUID = 2546262621080163608L;
@@ -12,15 +12,29 @@ public class UserList implements Serializable{
 	public UserList(){
 		UL = new ArrayList<User>();
 	}
+	/** Add user into the user list 
+	 * @param m
+	 */
 	public void addUser(User m){
 		UL.add(m);
 	}
+	/* (non-Javadoc)
+	 * custom toString method
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString(){
 		return UL.toString();
 	}
+	/**get list of users in data file 
+	 * @return
+	 */
 	public ArrayList<User> getList() {
 		return UL;
 	}
+	/**check if the userlist contains this user 
+	 * @param name
+	 * @return
+	 */
 	public boolean hasUser(String name) {
 		User t = new User(name);
 		if (UL.indexOf(t)>-1) {
@@ -28,13 +42,23 @@ public class UserList implements Serializable{
 		}
 		return false;
 	}
+	/**get user from the list of users 
+	 * @param name
+	 * @return
+	 */
 	public User getUser(String name) {
 		User t= new User(name);
 		return UL.get(UL.indexOf(t));
 	}
+	/**delete the user from the userlist 
+	 * @param name
+	 */
 	public void deleteUser(String name) {
 		UL.remove(new User(name));
 	}
+	/**alternate delete user from the userlist 
+	 * @param n
+	 */
 	public void deleteUser(User n) {
 		UL.remove(n);
 	}
